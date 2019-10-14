6ed7c46549a2dc7eb0a07487c67b6b0efadb5a7b
hamza essaadi <hamza.essaadi2014@gmail.com>
2019-10-14 23:09:22 +0100

    change route file

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
+//ici
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('/','DashboardController');
 Route::resource('/clients','ClientController');
